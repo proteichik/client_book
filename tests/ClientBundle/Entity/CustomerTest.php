62127524fcb6c810f75992dee1825f4cb7ebfb21
denis <proteichik@gmail.com>
2016-06-18 12:55:45 +0300

    add unit test for Customer

--- tests/ClientBundle/Entity/CustomerTest.php
+++ tests/ClientBundle/Entity/CustomerTest.php
@@ -11,6 +11,7 @@ use ClientBundle\Entity\Meeting;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
+
 class CustomerTest extends \PHPUnit_Framework_TestCase
 {
     private $customer;
@@ -37,5 +38,45 @@ class CustomerTest extends \PHPUnit_Framework_TestCase
     {
         $this->customer->setCompany('test company');
         $this->assertEquals('test company', $this->customer->getCompany());
+
+        //Invalid address type
+        try {
+            $this->customer->setAddress('invalid type');
+            $this->fail('Must throw exception');
+        } catch(\Exception $ex)
+        {
+
+        }
+
+        //Valid address type
+        $address = new Address();
+        $this->customer->setAddress($address);
+        $this->assertSame($address, $this->customer->getAddress());
+
+        //Invalid contact type
+        try {
+            $this->customer->setContacts('invalid contacts');
+            $this->fail('Must throw exception');
+        } catch(\Exception $ex) {
+
+        }
+
+        //Valid contact type
+        $contact = new Contacts();
+        $this->customer->setContacts($contact);
+        $this->assertSame($contact, $this->customer->getContacts());
+
+        $this->customer->setInfo('test info');
+        $this->assertEquals('test info', $this->customer->getInfo());
+
+        $call1 = new Call();
+        $call2 = new Call();
+        $this->customer->addCall($call1);
+        $this->customer->addCall($call2);
+        $this->assertEquals(2, $this->customer->getCalls()->count());
+
+        $this->customer->removeCall($call1);
+        $this->assertEquals(1, $this->customer->getCalls()->count());
+
     }
 }
