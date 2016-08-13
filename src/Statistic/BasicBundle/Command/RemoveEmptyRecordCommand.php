37a48abcaa30a174d25681c46179bd2a4086b3c1
denis <proteichik@gmail.com>
2016-08-13 20:27:43 +0300

    finish remove empty records command

--- src/Statistic/BasicBundle/Command/RemoveEmptyRecordCommand.php
+++ src/Statistic/BasicBundle/Command/RemoveEmptyRecordCommand.php
@@ -5,6 +5,8 @@ namespace Statistic\BasicBundle\Command;
 use Symfony\Component\Console\Command\Command;
 use Statistic\BasicBundle\Service\RecordService;
 use ClientBundle\Logger\LoggerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class RemoveEmptyRecordCommand extends Command
 {
@@ -27,6 +29,36 @@ class RemoveEmptyRecordCommand extends Command
     {
         $this->service = $service;
         $this->logger = $logger;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('statistic:remove:empty')
+            ->setDescription('Remove empty record from statistic');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->logger->info('Start command [statistic:remove:empty]');
+
+        $search_options = array(
+            'criteria' => array(
+                'countCalls' => 0,
+                'countMeetings' => 0,
+            ),
+        );
+        $records = $this->service->findBy($search_options);
+        $this->logger->info(sprintf('Total records = %s', count($records)));
+
+        foreach ($records as $record) {
+            $this->logger->info(sprintf('Record by %s has been removed', $record->getDate()->format('Y-m-d H:i:s')));
+            $this->service->remove($record, false);
+        }
+
+        $this->service->flush();
+        $this->logger->info('Finish command [statistic:remove:empty]');
     }
 
 
