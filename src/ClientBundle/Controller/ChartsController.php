ee31713cb79ef813f11f1ba3b249377bdc7017e6
denis <proteichik@gmail.com>
2016-08-16 20:48:59 +0200

    add getPieChart method

--- src/ClientBundle/Controller/ChartsController.php
+++ src/ClientBundle/Controller/ChartsController.php
@@ -3,6 +3,7 @@
 namespace ClientBundle\Controller;
 
 use ClientBundle\Service\ServiceInterface;
+use Ob\HighchartsBundle\Highcharts\Highchart;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use ClientBundle\Traits\FilterFormTrait;
@@ -32,8 +33,30 @@ class ChartsController extends Controller
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $qb = $this->service->getQueryBuilder('q');
+        $qb->select('SUM(q.countCalls) as countCalls')
+            ->addSelect('u.username')
+            ->join('q.user', 'u')
+            ->groupBy('q.user')
+        ;
+
+        $result = $qb->getQuery()->getResult();
+        var_dump($result);
 
 
         return $this->render('/charts/event.html.twig');
+    }
+
+    protected function getPieChart($name, array $data = array(), array $options = array())
+    {
+        $ob = new Highchart();
+        $ob->chart->renderTo('piechart');
+        $ob->title->text('Browser market shares at a specific website in 2010');
+        $ob->plotOptions->pie($options);
+        $ob->series(array(array('type' => 'pie','name' => $name, 'data' => $data)));
+    }
+
+    protected function getColumnChart()
+    {
+
     }
 }
