5723bafcbfbc713b47fdcab216ce872610211bbd
denis <proteichik@gmail.com>
2016-08-23 22:57:32 +0300

    refactor ChartController + add pie chart for meetings

---
+++ src/ClientBundle/Controller/Base/BaseChartController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace ClientBundle\Controller\Base;
+
+use ClientBundle\Factory\Charts\PieChartsFactory;
+use ClientBundle\Service\ServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ClientBundle\Traits\FilterFormTrait;
+
+abstract class BaseChartController extends Controller
+{
+    use FilterFormTrait;
+
+    /**
+     * @var ServiceInterface
+     */
+    protected $service;
+
+    /**
+     * CharsController constructor.
+     * @param ServiceInterface $service
+     * @param array $filterFormClasses
+     */
+    public function __construct(ServiceInterface $service, array $filterFormClasses = array())
+    {
+        $this->service = $service;
+        $this->filterFormClasses = $filterFormClasses;
+    }
+
+    protected function preparePieData($type, $data)
+    {
+        $result = array();
+
+        switch ($type)
+        {
+            case 'call':
+                foreach ($data as $item) {
+                    $result[] = array($item['username'], (int) $item['sumCalls']);
+                }
+                break;
+            case 'meeting':
+                foreach ($data as $item) {
+                    $result[] = array($item['username'], (int) $item['sumMeetings']);
+                }
+                break;
+            default:
+                $result = $data;
+        }
+
+        return $result;
+    }
+
+    protected function getPieOptions($type)
+    {
+        switch ($type)
+        {
+            case 'call':
+                return array(
+                    'renderTo' => 'piechart',
+                    'text' => 'Общее количество звонков',
+                    'name' => 'звонков',
+                );
+                break;
+            case 'meeting':
+                return array(
+                    'renderTo' => 'piechart',
+                    'text' => 'Общее количество встреч',
+                    'name' => 'встреч',
+                );
+                break;
+            default:
+                return array();
+        }
+    }
+
+    protected function getPieChart(array $data = array(), array $options = array())
+    {
+        $chartFactory = new PieChartsFactory();
+        return $chartFactory->getChart($data, $options);
+    }
+
+    protected function getColumnChart()
+    {
+
+    }
+}
--- src/ClientBundle/Controller/ChartsController.php
+++ src/ClientBundle/Controller/ChartsController.php
@@ -2,34 +2,12 @@
 
 namespace ClientBundle\Controller;
 
-use ClientBundle\Service\ServiceInterface;
-use Ob\HighchartsBundle\Highcharts\Highchart;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ClientBundle\Controller\Base\BaseChartController;
 use Symfony\Component\HttpFoundation\Request;
-use ClientBundle\Traits\FilterFormTrait;
 
-class ChartsController extends Controller
+class ChartsController extends BaseChartController
 {
-    use FilterFormTrait;
-
-    /**
-     * @var ServiceInterface
-     */
-    protected $service;
-
-    /**
-     * CharsController constructor.
-     * @param ServiceInterface $service
-     * @param array $filterFormClasses
-     */
-    public function __construct(ServiceInterface $service, array $filterFormClasses = array())
-    {
-        $this->service = $service;
-        $this->filterFormClasses = $filterFormClasses;
-    }
-    
-    
-    public function callPieAction(Request $request)
+    public function pieAction(Request $request, $type)
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -47,56 +25,12 @@ class ChartsController extends Controller
 
         $result = $qb->getQuery()->getResult();
 
-        $data = array();
-        foreach ($result as $item) {
-            $data[] = array($item['username'], (int) $item['sumCalls']);
-        }
-        $ob = $this->getPieChart('test', $data);
+        $data = $this->preparePieData($type, $result);
+        $ob = $this->getPieChart($data, $this->getPieOptions($type));
 
-        return $this->render('/charts/call_pie.html.twig', array('chart' => $ob, 'filterForm' => $filterForm->createView(),));
+        return $this->render('/charts/pie.html.twig', array('chart' => $ob, 'filterForm' => $filterForm->createView(),));
         
     }
 
-    public function eventAction(Request $request)
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        $qb = $this->service->getQueryBuilder('q');
-        $qb->select('SUM(q.countCalls) as countCalls')
-            ->addSelect('u.username')
-            ->join('q.user', 'u')
-            ->groupBy('q.user')
-        ;
-
-        $result = $qb->getQuery()->getResult();
-
-        $data = array();
-        foreach ($result as $item) {
-            $data[] = array($item['username'], (int) $item['countCalls']);
-        }
-        $ob = $this->getPieChart('test', $data);
-
-        return $this->render('/charts/event.html.twig', array('chart' => $ob));
-    }
-
-    protected function getPieChart($name, array $data = array(), array $options = array())
-    {
-        $ob = new Highchart();
-        $ob->chart->renderTo('piechart');
-        $ob->title->text('Browser market shares at a specific website in 2010');
-        $ob->plotOptions->pie(array(
-            'allowPointSelect'  => true,
-            'cursor'    => 'pointer',
-            'dataLabels'    => array('enabled' => false),
-            'showInLegend'  => true
-        ));
-        $ob->series(array(array('type' => 'pie','name' => $name, 'data' => $data)));
-
-        return $ob;
-    }
-
-    protected function getColumnChart()
-    {
-
-    }
 }
